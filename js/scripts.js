a15e7459380cf9d74c5b9eb3b238d2ec3622364c
lisa maccarrigan and zachary baum <zachary@>
2016-08-09 16:25:52 -0400

    add JS function to make vocab word appear

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,10 @@ $(".visible").click(function() {
   $(this).next().removeClass("hidden");
   $(this).next().addClass("visible");
 });
+
+$("p").click(function() {
+  $(this).removeClass("visible");
+  $(this).addClass("hidden");
+  $(this).prev().removeClass("hidden");
+  $(this).prev().addClass("visible");
+});
